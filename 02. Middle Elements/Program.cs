142bf7ae2f13fb96f560d54f46f7022dd96fce8a
Stefan Ivanov <135533109+Stef40fit@users.noreply.github.com>
2023-11-01 19:08:53 +0200

    Add project files.

---
+++ 02. Middle Elements/Program.cs
@@ -0,0 +1,14 @@
+﻿//•	Reads an array of integer numbers from the console, separated by single space
+//•	Array length will always be even number.
+//•	Calculate the average value of the elements in the middle of the array
+//•	Print the result formatted to the second digit
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int num1 = numbers[(numbers.Length /2)-1];
+int num2 = numbers[(numbers.Length /2) ];
+double averageValue = (num1 + num2) / 2.0;
+Console.WriteLine($"{averageValue:f2}");
+
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿//•	Reads an integer number from the console
+//•	Calculate sum of the factorials only on even digits of the given number
+//•	Print the calculated sum
+
+int result = int.Parse(Console.ReadLine());
+int totalSum = 0;
+while (result !=0)
+{
+    int digit = result % 10;
+    if(digit % 2 == 0)
+    {
+        totalSum += CalculateFactoriel(digit);
+    }
+    result = result / 10;
+}
+Console.WriteLine(totalSum);
+static int CalculateFactoriel (int number)
+{
+    int result = 1;
+    while (number > 0)
+    {
+        result *= number;
+        number--;
+    }
+    return result;
+}
